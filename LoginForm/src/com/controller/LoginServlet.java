cdbd679a0b684ffba187547ec8a0818e7b10966b
sunil965clear <sunilkumar192114@gmail.com>
2017-05-02 12:12:47 +0530

    Login using HTML, Servlet,JDBC

--- LoginForm/src/com/controller/LoginServlet.java
+++ LoginForm/src/com/controller/LoginServlet.java
@@ -15,48 +15,54 @@ import javax.servlet.http.HttpSession;
 
 import com.database.DbConnection;
 
-
 public class LoginServlet extends HttpServlet {
-	
-	Connection connect=null;
-	PreparedStatement statement=null;
-        @Override
-        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        	System.out.println("Login Called");
-    		
-    		response.setContentType("text/html");
-    		PrintWriter out = response.getWriter();
-    		
-    		String email = request.getParameter("user");
-    		String password = request.getParameter("pwd");
-    		
-    		new DbConnection();
-    		connect = DbConnection.connect();
-    		try {
-    			String query = "select * from Registration where email=? and password=?";
-    			statement = (PreparedStatement) connect.prepareStatement(query);
-    			statement.setString(1, email);
-    			statement.setString(2, password);
-    			ResultSet result = statement.executeQuery();
-    				if(result.next()){
-    					 out.print("Welcome, "+ email);  
-    				     HttpSession session=request.getSession();  
-    				     session.setAttribute("mailid",email);  
-    				     response.sendRedirect("profile");
-    				     
-    				     //request.getRequestDispatcher("/profile").forward(request, response);
-    				}
-    				else{
-    					 out.print("Sorry, Incorrect Emai_ID or Password.");  
-    			         request.getRequestDispatcher("/index.html").include(request, response);  
-    				}
-    			
-    		} catch (SQLException e) {
-    			e.printStackTrace();
-    		}
-    		
-        }
 
-	
-	
+	private static final long serialVersionUID = 1L;
+	Connection connect = null;
+	PreparedStatement statement = null;
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("Login Called");
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		String email = request.getParameter("user");
+		String password = request.getParameter("pwd");
+
+		new DbConnection();
+		connect = DbConnection.connect();
+		try {
+			String query = "select * from Registration where email=? and password=?";
+			statement = (PreparedStatement) connect.prepareStatement(query);
+			statement.setString(1, email);
+			statement.setString(2, password);
+			ResultSet result = statement.executeQuery();
+			if (result.next()) {
+				out.print("Welcome, " + email);
+				HttpSession session = request.getSession();
+				session.setAttribute("mailid", email);
+				response.sendRedirect("profile");
+
+				// request.getRequestDispatcher("/profile").forward(request,
+				// response);
+			} else {
+				out.print("Sorry, Incorrect Emai_ID or Password.");
+				request.getRequestDispatcher("/index.html").include(request, response);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			connect.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 }
--- LoginForm/src/com/controller/LogoutServlet.java
+++ LoginForm/src/com/controller/LogoutServlet.java
@@ -10,23 +10,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet(urlPatterns="/LogoutServlet")
+@WebServlet(urlPatterns = "/LogoutServlet")
 public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-  
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		  response.setContentType("text/html");  
-          PrintWriter out=response.getWriter();  
-          
-          HttpSession session=request.getSession();  
-          session.invalidate(); 
-          request.getSession();
-          
-          
-           //request.getRequestDispatcher("link.html").include(request, response);  
-          out.print("You are successfully logged out!");  
-          out.close();
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		HttpSession session = request.getSession();
+		session.invalidate();
+		request.getSession();
+
+		out.print("You are successfully logged out!");
+		request.getRequestDispatcher("index.html").include(request, response);
+		out.close();
 	}
 
 }
